356c5de613be9d865e642d1f568d3b180776d876
root <root@v2202207179317195569.megasrv.de>
2024-09-19 16:40:38 +0200

    remove sensitive data

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,8 +10,5 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "122ec87c-07b5-46a5-bbbb-1a7540bec89e"
-  client_secret   = "KSh8Q~AwDcVy3mwDytn5jkNmcRp3soV1jdoKTaey"
-  tenant_id       = "919cd4d3-f836-4bbc-9bc0-a14661caa4af"
   subscription_id = var.azure_subscription_id
 }
